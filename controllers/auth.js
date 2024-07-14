9481dded84d85d9c06e91ec3b3cf0a62568241d3
Joaquin <joaquincarena5@gmail.com>
2024-07-14 19:00:19 -0300

    first commit

---
+++ controllers/auth.js
@@ -0,0 +1,20 @@
+const {response} = require('express');
+const { validationResult } = require('express-validator');
+
+const createUser = (req, res = response) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty) {
+        return res.status(400).json({
+            ok: false,
+            errors: errors.mapped
+        })
+    }
+
+    res.json({
+        ok: true,
+        msg: 'Creating user!'
+    });
+}
+
+module.exports = {createUser}
